c9e3e23fd2575a3654c71846d32ffa8799997e7e
Useradm <useradm@localhost.localdomain>
2020-05-12 06:38:51 +0000

    Third Commit

--- deployer.sh
+++ deployer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash 
-#En supposant que tu fait le clonage dans votre répertoire personnelle.
-#git clone  https://github.com/HamzaDardouri/SecureLogin.git
+#En supposant que tu fait le clonage dans votre répertoire personnelledu machine Apache_PHP
 #Pour faire le déploiement:
-sudo cp -r  'SecureLogin'  '/var/www/html/'
+sudo cp -r  'SecureLoginLamp_2tiers'  '/var/www/html/'
 chmod +x  'deployer2.sh' | scp -r -p  'deployer2.sh'  'useradm'@'192.168.56.23':~
 echo "Successful"
 exit
